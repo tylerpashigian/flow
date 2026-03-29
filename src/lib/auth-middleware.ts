f802c97e0c16958429deaa53f33cd434336c901e
Tyler Pashigian <tylerpashigian@gmail.com>
2026-03-29 16:54:25 -0500

    Adding auth middleware

---
+++ src/lib/auth-middleware.ts
@@ -0,0 +1,16 @@
+import { createMiddleware } from '@tanstack/react-start'
+import { getRequest } from '@tanstack/react-start/server'
+import { authClient } from './auth-client'
+export const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const { data: session } = await authClient.getSession({
+    fetchOptions: {
+      headers: getRequest().headers,
+    },
+  })
+
+  return await next({
+    context: {
+      user: { id: session?.user.id },
+    },
+  })
+})
